2e43b97d507b1bfa6058343c7ca39c0e9744cfc0
Artem Kuzmenko <arkuzka@mail.ru>
2020-08-22 13:20:02 +0300

    port 15000

--- server.py
+++ server.py
@@ -108,7 +108,7 @@ def message_received(client, server, message):
     server.send_message_to_all(client_list_json)
 
 
-PORT = 8080
+PORT = 15000
 print('Server run')
 server = WebsocketServer(PORT)
 server.set_fn_new_client(new_client)
